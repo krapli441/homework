b63ec2217303faaa8a7ee577db2dd971a668ab92
박준형 <108733795+krapli441@users.noreply.github.com>
2024-09-04 16:01:40 +0900

    App.tsx - Todo 컴포넌트 및 useTodo 훅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+import useTodos from "./hooks/useTodo";
 
-const App = () => <div>TODO LIST</div>;
+const App: React.FC = () => {
+  const { todos, addTodo, toggleTodo, deleteTodo } = useTodos();
+
+  return (
+    <div className="App">
+      <h1>ToDo 리스트</h1>
+      <TodoInput addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
 
 export default App;
