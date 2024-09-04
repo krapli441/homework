c4f02fefa4041d1b6b2fcd1bd97e66d5e4dc552d
박준형 <108733795+krapli441@users.noreply.github.com>
2024-09-04 16:17:47 +0900

    TodoInput - 엔터 키를 입력했을 때에도 투두를 추가하도록 변경

--- src/components/TodoInput.tsx
+++ src/components/TodoInput.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 interface TodoInputProps {
   addTodo: (text: string) => void;
 }
 
 const TodoInput: React.FC<TodoInputProps> = ({ addTodo }) => {
-  const [inputValue, setInputValue] = useState("");
+  const [inputValue, setInputValue] = useState('');
 
   const handleAddTodo = () => {
     if (inputValue.trim()) {
       addTodo(inputValue);
-      setInputValue("");
+      setInputValue('');
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleAddTodo();
     }
   };
 
@@ -20,6 +26,7 @@ const TodoInput: React.FC<TodoInputProps> = ({ addTodo }) => {
         type="text"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
+        onKeyPress={handleKeyPress} // 'Enter' 키 입력 감지
         placeholder="할 일을 입력하세요"
       />
       <button onClick={handleAddTodo}>추가</button>
