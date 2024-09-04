8697f36f00d861cce9019ad852f1c401add799b3
박준형 <108733795+krapli441@users.noreply.github.com>
2024-09-04 15:55:57 +0900

    TodoList.tsx 로직 작성

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TodoListProps {
+  todos: Todo[];
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  toggleTodo,
+  deleteTodo,
+}) => {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          toggleTodo={toggleTodo}
+          deleteTodo={deleteTodo}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
