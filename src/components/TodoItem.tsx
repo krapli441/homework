b9ffab91c51aa2e42ebd6de35be64c26d6b6dc85
박준형 <108733795+krapli441@users.noreply.github.com>
2024-09-05 14:39:00 +0900

    todoitem - 삭제 텍스트 아이콘으로 변경

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { List, Checkbox, Button } from "antd";
+import { DeleteOutlined } from "@ant-design/icons"; // Ant Design 아이콘 import
 
 interface Todo {
   id: number;
@@ -21,9 +22,18 @@ const TodoItem: React.FC<TodoItemProps> = ({
   return (
     <List.Item
       actions={[
-        <Button type="link" danger onClick={() => deleteTodo(todo.id)}>
-          삭제
-        </Button>,
+        <Button
+          type="primary"
+          danger
+          shape="circle"
+          icon={<DeleteOutlined />}
+          onClick={() => deleteTodo(todo.id)}
+          style={{
+            backgroundColor: "red",
+            borderColor: "red", 
+            color: "white",
+          }}
+        />,
       ]}
     >
       <Checkbox checked={todo.completed} onChange={() => toggleTodo(todo.id)}>
